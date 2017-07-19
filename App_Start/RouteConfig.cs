8971e159c708834ee29b3e0fc65123b98d7ba1af
Katherine Trunkey <ktrunkey@gmail.com>
2017-07-19 16:28:53 -0700

    Initial commit for MVC website files

---
+++ App_Start/RouteConfig.cs
@@ -0,0 +1,54 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GoldSim.Web
+{
+
+  /*============================================================================================================================
+  | CLASS: ROUTE CONFIGURATION
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides default routing configuration for MVC.
+  /// </summary>
+  public class RouteConfig {
+
+    /*==========================================================================================================================
+    | METHOD: REGISTER ROUTES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provided a <see cref="RouteCollection"/>, registers all routes associated with the application.
+    /// </summary>
+    /// <param name="routes">
+    ///   The route collection for the server, typically passed from the <see cref="System.Web.HttpApplication"/> class.
+    /// </param>
+    public static void RegisterRoutes(RouteCollection routes) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Ignore requests to AXDs (handled by HttpHandler)
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Enable attribute-based routing
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      routes.MapMvcAttributeRoutes();
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Handle default route convention
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      routes.MapRoute(
+        name: "Default",
+        url: "{controller}/{action}/{id}",
+        defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+      );
+
+    }
+
+  }
+
+}
---
+++ Global.asax.cs
@@ -0,0 +1,129 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System;
+using System.Configuration;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Ignia.Topics;
+using Ignia.Topics.Data.Caching;
+using Ignia.Topics.Data.Sql;
+using Ignia.Topics.Web;
+using Ignia.Web.Tools;
+
+namespace GoldSim.Web {
+
+  /*============================================================================================================================
+  | CLASS: GLOBAL
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides default configuration for the application, including any special processing that needs to happen relative to 
+  ///   application events (such as <see cref="Application_Start"/> or <see cref="System.Web.HttpApplication.Error"/>.
+  /// </summary>
+  public class Global : HttpApplication {
+
+    /*==========================================================================================================================
+    | METHOD: APPLICATION START (EVENT HANDLER)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides initial configuration for the application, including registration of MVC routes via the 
+    ///   <see cref="RouteConfig"/> class.
+    /// </summary>
+    void Application_Start(object sender, EventArgs e) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Initialize Topic Repository
+      >-------------------------------------------------------------------------------------------------------------------------
+      | ### NOTE JJC071517: In order to maintain forward compatibility with dependency injection, the legacy ASP.NET Web Forms 
+      | support requires that the TopicRepository be configured with a concrete instance of an ITopicRepository instance. 
+      | (Previously, this relied instead on the ASP.NET Provider Model, which prevented injection of e.g. connection strings and
+      | thus established hard-coded dependencies on e.g. ConfigurationManager)
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var connectionString      = ConfigurationManager.ConnectionStrings["TopicsServer"].ConnectionString;
+      var sqlTopicRepository    = new SqlTopicRepository(connectionString);
+      var cachedTopicRepository = new CachedTopicRepository(sqlTopicRepository);
+
+      TopicRepository.DataProvider      = cachedTopicRepository;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Register routes
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+    }
+
+    /*==========================================================================================================================
+    | METHOD: REGISTER ROUTES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Establishes (non-MVC) URL routes and maps them to the appropriate page handlers.
+    /// </summary>
+    /// <param name="routes">
+    ///   The route collection for the server, typically passed from the <see cref="System.Web.HttpApplication"/> class.
+    /// </param>
+    void RegisterRoutes(RouteCollection routes) {
+
+      routes.MapPageRoute("Edit",       "Edit/{*directory}",    "~/!Admin/Topics/Default.aspx");
+      routes.MapPageRoute("Legacy",     "Content.asp",          "~/Redirector.aspx");
+      routes.MapPageRoute("Redirect",   "Topic/{topicID}",      "~/Redirector.aspx");
+
+      routes.Add(
+        "TopicUrl",
+        new Route(
+          "{namespace}/{*path}",
+          new TopicsRouteHandler()
+        )
+      );
+
+    }
+
+    /*==========================================================================================================================
+    | EVENT: APPLICATION ERROR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Handles errors that are not otherwise handled by the page_error object or inline error handling code.
+    /// </summary>
+    void Application_Error(Object sender, EventArgs e) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Handle HTTP exceptions (namely, 404s)
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (Server.GetLastError().GetType() == typeof(HttpException)) {
+        switch (((HttpException)Server.GetLastError()).GetHttpCode()) {
+          case 404:
+            Server.ClearError();
+            Server.Transfer(
+              "/Common/Error.Pages/404.aspx?"
+              + "PagePath=" + Server.UrlEncode(Context.Request.ServerVariables["SCRIPT_NAME"])
+              + "&QueryString=" + Server.UrlEncode(Context.Request.QueryString.ToString())
+            );
+            return;
+            break;
+          default:
+            break;
+        }
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Handle querystring/form hacks
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (Server.GetLastError().GetType() == typeof(System.Web.HttpRequestValidationException)) {
+        Server.ClearError();
+        Server.Transfer("/Common/Error.Pages/XSS.aspx");
+        return;
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Clear error and redirect to a friendly error page
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Server.ClearError();
+      Server.Transfer("/Common/Error.Pages/Exception.aspx");
+
+    }
+
+  }
+
+}
