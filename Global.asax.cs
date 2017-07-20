3143e69e740ac2141e0d8a82c6c7742b495e1152
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-07-19 19:05:33 -0700

    Removed trailing spaces

--- Global.asax.cs
+++ Global.asax.cs
@@ -20,7 +20,7 @@ namespace GoldSim.Web {
   | CLASS: GLOBAL
   \---------------------------------------------------------------------------------------------------------------------------*/
   /// <summary>
-  ///   Provides default configuration for the application, including any special processing that needs to happen relative to 
+  ///   Provides default configuration for the application, including any special processing that needs to happen relative to
   ///   application events (such as <see cref="Application_Start"/> or <see cref="System.Web.HttpApplication.Error"/>.
   /// </summary>
   public class Global : HttpApplication {
@@ -29,7 +29,7 @@ namespace GoldSim.Web {
     | METHOD: APPLICATION START (EVENT HANDLER)
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
-    ///   Provides initial configuration for the application, including registration of MVC routes via the 
+    ///   Provides initial configuration for the application, including registration of MVC routes via the
     ///   <see cref="RouteConfig"/> class.
     /// </summary>
     void Application_Start(object sender, EventArgs e) {
@@ -37,14 +37,14 @@ namespace GoldSim.Web {
       /*------------------------------------------------------------------------------------------------------------------------
       | Initialize Topic Repository
       >-------------------------------------------------------------------------------------------------------------------------
-      | ### NOTE JJC071517: In order to maintain forward compatibility with dependency injection, the legacy ASP.NET Web Forms 
-      | support requires that the TopicRepository be configured with a concrete instance of an ITopicRepository instance. 
+      | ### NOTE JJC071517: In order to maintain forward compatibility with dependency injection, the legacy ASP.NET Web Forms
+      | support requires that the TopicRepository be configured with a concrete instance of an ITopicRepository instance.
       | (Previously, this relied instead on the ASP.NET Provider Model, which prevented injection of e.g. connection strings and
       | thus established hard-coded dependencies on e.g. ConfigurationManager)
       \-----------------------------------------------------------------------------------------------------------------------*/
-      var connectionString      = ConfigurationManager.ConnectionStrings["TopicsServer"].ConnectionString;
-      var sqlTopicRepository    = new SqlTopicRepository(connectionString);
-      var cachedTopicRepository = new CachedTopicRepository(sqlTopicRepository);
+      var connectionString              = ConfigurationManager.ConnectionStrings["TopicsServer"].ConnectionString;
+      var sqlTopicRepository            = new SqlTopicRepository(connectionString);
+      var cachedTopicRepository         = new CachedTopicRepository(sqlTopicRepository);
 
       TopicRepository.DataProvider      = cachedTopicRepository;
 
