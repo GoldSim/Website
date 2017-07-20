2e6efb267f03c1ea4890efc2603f191544499548
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-07-19 19:04:44 -0700

    Established Controller Factory
    The Controller Factory is the Composition Root for MVC. In this case, it
    relies on the legacy (not DI) TopicRepository class for backward
    compatibility, although that implements the (DI) ITopicRepository, which
    is configured in the global.asax.

---
+++ GoldSimControllerFactory.cs
@@ -0,0 +1,64 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using GoldSim.Web.Controllers;
+using Ignia.Topics;
+using Ignia.Topics.Web;
+using Ignia.Topics.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace GoldSim.Web {
+
+  /*============================================================================================================================
+  | CLASS: HOME CONTROLLER
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides access to the default homepage for the site.
+  /// </summary>
+  class GoldSimControllerFactory : DefaultControllerFactory {
+
+    /*==========================================================================================================================
+    | GET CONTROLLER INSTANCE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Overrides the factory method for creating new instances of controllers.
+    /// </summary>
+    /// <returns>A concrete instance of an <see cref="IController"/>.</returns>
+    protected override IController GetControllerInstance(RequestContext requestContext, Type controllerType) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Register
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var topicRepository       = TopicRepository.DataProvider;
+      var rootTopic             = TopicRepository.RootTopic;
+      var topicRoutingService   = new TopicRoutingService(topicRepository, requestContext);
+      //var topicViewModel        = new TopicViewModel(topicRepository, topicRoutingService.Topic);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Resolve
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (topicRoutingService.Topic != null) {
+        return new TopicController<Topic>(topicRepository, topicRoutingService.Topic);
+      }
+      else {
+        return new TopicController<Topic>(topicRepository, rootTopic.GetTopic("Web:Home"));
+      }
+
+      //return base.GetControllerInstance(requestContext, controllerType);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Release
+      \-----------------------------------------------------------------------------------------------------------------------*/
+
+    }
+
+  } //Class
+} //Namespace
