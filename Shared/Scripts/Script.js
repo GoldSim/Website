e7e3a2c77cf13b720b1899d5a59b980fdcd1dc65
Katherine Trunkey <ktrunkey@gmail.com>
2017-08-01 08:50:25 -0700

    Established base scripts file with jQuery load functionality
    Set up script file with goldSimWeb namespace; included load function for
    setting full pane height.

---
+++ Shared/Scripts/Script.js
@@ -0,0 +1,33 @@
+﻿/**
+ * (GOLDSIM WEB) SCRIPTS
+ * @file Defines functions, primarily presentation-oriented, related to the GoldSim website.
+ * @namespace goldSimWeb
+ */
+
+/**
+ * jQuery functionality
+ */
+(function(goldSimWeb, $, undefined) {
+  'use strict';
+
+  $(document).ready(function() {
+
+    /**
+     * Establish variables
+     */
+    var
+      $window                   = $(window),
+      $siteHeader               = $('header.site.header'),
+      $paneFullHeight           = ($window.height() - $siteHeader.height());
+
+    /**
+     * Set elements marked with class "js-full-height" to 100% of the viewport height, minus the top bar
+     */
+    $('.js-full-height').each(function() {
+      var $this                 = $(this);
+      $this.innerHeight($paneFullHeight);
+    });
+
+  });
+
+}(window.goldSimWeb = window.goldSimWeb || {}, jQuery));
