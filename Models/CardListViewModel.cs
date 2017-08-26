7322ef8b63d50fcf4b943a41f153795f96536b7b
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-08-26 14:40:16 -0700

    Centralized card list logic
    Introduced a CardListViewModel and a _CardList.cshtml to centralize the
    card list logic. Implemented on the ApplicationIndex and ApplicationPage
    views.

---
+++ Models/CardListViewModel.cs
@@ -0,0 +1,70 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ignia.Topics;
+using Ignia.Topics.Repositories;
+using Ignia.Topics.Web.Mvc;
+
+namespace GoldSim.Web.Models {
+
+  /*============================================================================================================================
+  | CLASS: CARD LIST VIEW MODEL
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   A view model for rendering a list of card objects.
+  /// </summary>
+  public class CardListViewModel {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private List<Topic> _cards = null;
+    private string _className = null;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a Card List View Model with appropriate dependencies.
+    /// </summary>
+    /// <returns>A card list view model.</returns>
+    public CardListViewModel(List<Topic> cards, string className="") {
+      _cards = cards;
+      _className = className;
+    }
+
+    /*==========================================================================================================================
+    | CARDS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a list of cards to be rendered as part of the card list.
+    /// </summary>
+    /// <returns>A <see cref="List{T}"/> of <see cref="Ignia.Topics.Topic"/>, each representing a unique card.</returns>
+    public List<Topic> Cards {
+      get {
+        return _cards;
+      }
+    }
+
+    /*==========================================================================================================================
+    | CLASS NAME
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides the (optional) CSS class to be associated with each card.
+    /// </summary>
+    /// <returns>A CSS class name.</returns>
+    public string ClassName {
+      get {
+        return _className;
+      }
+    }
+
+  } //Class
+} //Namespace
