616e3acdc8a4a1e8ecd3b1be8030b62c168953a0
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-08-28 15:10:43 -0700

    Added fix for accordion scrolling
    If a user opens an panel in an accordion after a particularly long
    panel, the content will not be visible. To fix this, I added a script
    that, upon click, will scroll to the top of the accordion panel.

---
+++ Shared/Scripts/Accordion.ScrollFix.js
@@ -0,0 +1,9 @@
+﻿(function(goldSimWeb, $, undefined) {
+  $(".accordion").on("up.zf.accordion", function (event) {
+    var accordion = $(this);
+    setTimeout(function () {
+      $('html,body').animate({ scrollTop: $(accordion).find('.is-active').offset().top }, 'slow');
+    },
+    250); //Set to slideSpeed
+  });
+} (window.goldSimWeb = window.goldSimWeb || {}, jQuery));
