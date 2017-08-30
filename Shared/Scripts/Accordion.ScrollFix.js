5b76ca6cc643dfcbe61e18e8e3eac623bc895293
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-08-29 19:56:50 -0700

    Allowed animations to run in parallel
    Scroll to top previously occurred _after_ the accordion animation,
    making for a jumpy experience. Modified the behavior so it happen
    _simultaneously_.

--- Shared/Scripts/Accordion.ScrollFix.js
+++ Shared/Scripts/Accordion.ScrollFix.js
@@ -1,9 +1,25 @@
-﻿(function(goldSimWeb, $, undefined) {
-  $(".accordion").on("up.zf.accordion", function (event) {
+﻿(function (goldSimWeb, $, undefined) {
+
+  var _previousAccordion = null;
+  var _previousHeight = 0;
+  var _previousTop = window.innerHeight;
+
+  $(".accordion").on("down.zf.accordion", function (event) {
+    _previousAccordion = $(this).find(".is-active .accordion-content");
+    _previousHeight = _previousAccordion.outerHeight();
+    _previousTop = _previousAccordion.offset().top;
+  });
+
+  $("li.accordion-item").on("click", function (event) {
     var accordion = $(this);
-    setTimeout(function () {
-      $('html,body').animate({ scrollTop: $(accordion).find('.is-active').offset().top }, 'slow');
-    },
-    250); //Set to slideSpeed
+    var offset = 0;
+    if (!accordion.hasClass("is-active")) {
+      _previousTop = window.innerHeight;
+      return;
+    }
+    if (_previousTop < accordion.offset().top) {
+      offset = _previousHeight;
+    }
+    $('html,body').animate({ scrollTop: $(accordion).offset().top - offset }, 'fast');
   });
 } (window.goldSimWeb = window.goldSimWeb || {}, jQuery));
