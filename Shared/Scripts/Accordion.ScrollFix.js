ec57731d3e83fb6b0578bd440a369249b702cb5b
Jeremy Caney <Jeremy.Caney@Ignia.com>
2017-08-30 14:56:20 -0700

    Centralized top variable definition

--- Shared/Scripts/Accordion.ScrollFix.js
+++ Shared/Scripts/Accordion.ScrollFix.js
@@ -8,20 +8,21 @@
   $(".accordion").on("down.zf.accordion", function (event) {
     _previousAccordion = $(this);
     _previousAccordionItem = $(this).find(".is-active .accordion-content");
-    _previousHeight = _previousAccordionItem.outerHeight();
+    _previousHeight = _previousAccordionItem.innerHeight();
     _previousTop = _previousAccordionItem.offset().top;
   });
 
   $("li.accordion-item").on("click", function (event) {
     var accordionItem = $(this);
+    var top = $(accordionItem).offset().top;
     var offset = 0;
     if (!accordionItem.hasClass("is-active")) {
       _previousTop = window.innerHeight;
       return;
     }
-    if (_previousTop < accordionItem.offset().top && _previousAccordion && _previousAccordion.has(accordionItem).length) {
+    if (_previousAccordion && _previousTop < top && _previousAccordion.has(accordionItem).length) {
       offset = _previousHeight;
     }
-    $('html,body').animate({ scrollTop: $(accordionItem).offset().top - offset }, 'fast');
+    $('html,body').animate({ scrollTop: top - offset }, 'fast');
   });
 } (window.goldSimWeb = window.goldSimWeb || {}, jQuery));
