c38ef7074b0aefe7974e88ebd8209db56b3e9f6a
Katherine Trunkey <ktrunkey@gmail.com>
2017-09-19 15:08:46 -0700

    Distributed JavaScripts into individual files
    Additionally concatenated, minified, and uglified the scripts via gulp,
    output to Scripts.js, and updated the primary script reference in the
    _Layout.cshtml.

---
+++ Shared/Scripts/Layout.js
@@ -0,0 +1,35 @@
+﻿/**
+ * (GOLDSIM WEB) LAYOUT SCRIPTS
+ * @file Defines presentation-oriented functionality related layout concerns for the GoldSim website.
+ * @namespace goldSimWeb
+ */
+(function (goldSimWeb, $, undefined) {
+  'use strict';
+
+  $(document).ready(function () {
+
+    /**
+     * Establish variables
+     */
+    var
+      $window                   = $(window),
+      $siteHeader               = $('header.site.header'),
+      $screenSize               = $window.width(),
+      $paneFullHeight           = ($window.height() - $siteHeader.height());
+
+    /**
+     * Set elements marked with class "js-full-height" to 100% of the viewport height, minus the top bar
+     */
+    $('.js-full-height').each(function () {
+      var $this = $(this);
+      if ($window.width() > 768) {
+        $this.innerHeight($paneFullHeight);
+      }
+    });
+
+    // ### HACK KLT 20170807: Temporary workaround for stylesheet prototype
+    $('</section>').prependTo($('.panel.body section.panel.accordion'));
+
+  });
+
+}(window.goldSimWeb = window.goldSimWeb || {}, jQuery));
---
+++ Shared/Scripts/SearchBar.js
@@ -0,0 +1,38 @@
+﻿/**
+ * (GOLDSIM WEB) SEARCH BAR SCRIPTS
+ * @file Defines functionality associated with the expanding search bar in the upper right of the GoldSim website.
+ * @namespace goldSimWeb
+ */
+(function (goldSimWeb, $, undefined) {
+  'use strict';
+
+  $(document).ready(function () {
+
+    /**
+     * Add placeholder text to GCSE input
+     */
+    setTimeout(function () {
+      $('#gsc-i-id1').attr('placeholder', 'Search');
+    }, 2000);
+
+    /**
+     * Capture search button trigger, set open/closed state
+     */
+    $('.search.form .buttons button').mousedown(function (event) {
+      var $searchBar = $('div.search.form');
+      if ($searchBar.hasClass('closed')) {
+        event.preventDefault();
+        $('div.search.form').addClass('open').removeClass('closed');
+        $('input.gsc-input').focus();
+      }
+      else {
+        $('div.search.form').addClass('closed').removeClass('open');
+      }
+    });
+    $('.search.form').on('blur', 'input.gsc-input', function (event) {
+      $('div.search.form').addClass('closed').removeClass('open');
+    });
+
+  });
+
+}(window.goldSimWeb = window.goldSimWeb || {}, jQuery));
