f66ff9bd85d9e0c6f1c4a6538a93929a5ec314e5
Katherine Trunkey <ktrunkey@gmail.com>
2017-09-21 14:24:02 -0700

    Introduced image zoom modal functionality
    Added scripts to define a button for opening a Foundation Reveal modal
    as well as the modal markup and manual trigger.

---
+++ Shared/Scripts/Images.Zoom.js
@@ -0,0 +1,50 @@
+﻿/**
+ * (GOLDSIM WEB) IMAGES ZOOM SCRIPTS
+ * @file Defines functionality and presentation elements needed for zooming in on an image on the GoldSim website.
+ * @namespace goldSimWeb
+ */
+(function (goldSimWeb, $, undefined) {
+  'use strict';
+
+  $(document).ready(function() {
+
+    /**
+     * Loop through all zoomable items on the page in order to: wrap and append zoom link; add modal element for full-size
+     * image.
+     */
+    $('.zoomable').each(function() {
+
+      // Establish variables
+      var
+        zoomableItem            = $(this),
+        zoomableImage           = $(zoomableItem).find('img').attr('src'),
+        imageNameStart          = (zoomableImage.lastIndexOf('/') + 1),
+        imageNameEnd            = zoomableImage.lastIndexOf('.'),
+        imageName               = zoomableImage.substring(imageNameStart, imageNameEnd),
+        imageModal              = '<div class="reveal" id="' + imageName + '" data-reveal data-close-on-click="true"><img src="' + zoomableImage + '" alt="*" /><button class="close-button" data-close aria-label="Close Modal" type="button"><span aria-hidden="true">&times;</span></button></div>',
+        wrapper                 = '<div class="zoomable wrapper"></div>';
+      if ($(zoomableItem).hasClass('callout')) {
+        wrapper                 = '<div class="callout zoomable wrapper"></div>';
+      }
+
+      // Add wrapper and button to zoomable item
+      $(zoomableItem).wrap(wrapper).after('<button class="js-zoom" data-open="' + imageName + '" data-toggle="' + imageName + '">Zoom In <i class="fa fa-search-plus"></i></button>');
+
+      // Add modal based on zoomable item's image
+      $('body').append(imageModal);
+
+    });
+
+    /**
+     * Open Foundation Reveal on modal button click
+     */
+    $('button.js-zoom').click(function () {
+      var
+        modalToOpen             = $(this).data('open');
+        revealModal             = new Foundation.Reveal($('#' + modalToOpen));
+        revealModal.open();
+    });
+
+  });
+
+}(window.goldSimWeb = window.goldSimWeb || {}, jQuery));
