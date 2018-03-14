6783154dae01aa81c835a607786605b87325f693
Katherine Trunkey <ktrunkey@gmail.com>
2018-03-14 16:36:11 -0700

    Added interface for use by specific reporting service classes

---
+++ App_Code/IReportingService.cs
@@ -0,0 +1,46 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System.Collections.Generic;
+using System.IO;
+using Ignia.Topics;
+
+namespace GoldSim.Web {
+
+  /*============================================================================================================================
+  | INTERFACE: REPORTING SERVICE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Given contextual information (such as mime and file type), will determine the current reporting method to return.
+  /// </summary>
+  public interface IReportingService {
+
+    /*==========================================================================================================================
+    | MIME TYPE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets the mime type associated with the reporting method output file.
+    /// </summary>
+    string MimeType { get; }
+
+    /*==========================================================================================================================
+    | FILE EXTENSION
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets the file extension associated with the reporting method output file.
+    /// </summary>
+    string FileExtension { get; }
+
+    /*==========================================================================================================================
+    | GET LICENSE REQUESTS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets the License Requests Topics data as a memory stream for download.
+    /// </summary>
+    MemoryStream GetLicenseRequests(IEnumerable<Topic> licenseRequests);
+
+  } // Interface
+
+} // Namespace
