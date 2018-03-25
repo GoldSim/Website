d5b892bad74b840acd2c8d3a5c0da175d63aa189
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-03-25 16:35:13 -0700

    Added `ModulePageTopic`
    In order for modules to be treated as cards, they need to implement the `ICardViewModel` interface. Currently, modules are `Page` content types. This will require that they be changed to a new `ModulePage` content type, which is simply a wrapper for `Page` (it can have the same data model, except it'll add `ThumbnailImage`).

---
+++ Models/ModulePageTopicViewModel.cs
@@ -0,0 +1,25 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Goldsim
+| Project       Website
+\=============================================================================================================================*/
+using Ignia.Topics.ViewModels;
+using Ignia.Topics.Mapping;
+using System.ComponentModel;
+
+namespace GoldSim.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: MODULE PAGE TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a <c>ModulePage</c>
+  ///   topic.
+  /// </summary>
+  public class ModulePageTopicViewModel : PageTopicViewModel, ICardViewModel {
+
+    public string ThumbnailImage { get; set; }
+
+  } // Class
+
+} // Namespace
