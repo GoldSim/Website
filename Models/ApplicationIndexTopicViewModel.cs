5dfba7c6ec4a00dda10876d5dd6c7e1d41fcb578
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-03-25 15:25:02 -0700

    Added `*ApplicationIndexTopicViewModel`s

---
+++ Models/ApplicationIndexTopicViewModel.cs
@@ -0,0 +1,27 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Goldsim
+| Project       Website
+\=============================================================================================================================*/
+using Ignia.Topics.ViewModels;
+using Ignia.Topics.Mapping;
+using System.ComponentModel;
+
+namespace GoldSim.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: APPLICATION INDEX TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a <c>ApplicationIndex</c> topic.
+  /// </summary>
+  public class ApplicationIndexTopicViewModel : TopicViewModel {
+
+    public string FilteredDocumentType { get; set; }
+    public TopicViewModelCollection<ApplicationBasePageTopicViewModel> EnvironmentalApplications { get; set; }
+    public TopicViewModelCollection<ApplicationBasePageTopicViewModel> BusinessApplications { get; set; }
+    public TopicViewModelCollection<ApplicationBasePageTopicViewModel> SystemsApplications { get; set; }
+
+  } // Class
+
+} // Namespace
---
+++ Models/ExampleApplicationIndexTopicViewModel.cs
@@ -0,0 +1,24 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Goldsim
+| Project       Website
+\=============================================================================================================================*/
+using Ignia.Topics.ViewModels;
+using Ignia.Topics.Mapping;
+using System.ComponentModel;
+
+namespace GoldSim.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: EXAMPLE APPLICATION INDEX TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a <c>ExampleApplicationIndex</c>
+  ///   topic.
+  /// </summary>
+  public class ExampleApplicationIndexTopicViewModel: ApplicationIndexTopicViewModel {
+
+
+  } // Class
+
+} // Namespace
