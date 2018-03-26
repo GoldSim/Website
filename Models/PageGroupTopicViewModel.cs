25666505d5fcf9b66e66d17c81793b26a0ee035f
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-03-25 18:51:41 -0700

    Introduced override of `PageGroupTopicViewModel`
    This allows the `ICardViewModel` interface to be implemented, thus allowing the e.g. module pages (or any other page group, technically) to be treated as a card).

---
+++ Models/PageGroupTopicViewModel.cs
@@ -0,0 +1,30 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Goldsim
+| Project       Website
+\=============================================================================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GoldSim.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: PAGE GROUP TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a page group topic.
+  /// </summary>
+  /// <remarks>
+  ///   There is already a centralized <see cref="Ignia.Topics.ViewModels.PageGroupTopicViewModel"/>. It doesn't implement the
+  ///   necessary <see cref="ICardViewModel"/> interface needed for e.g. Modules to be treated as cards.
+  /// </remarks>
+  public class PageGroupTopicViewModel : Ignia.Topics.ViewModels.PageGroupTopicViewModel, ICardViewModel {
+
+    public string ThumbnailImage { get; set; }
+    public string WebPath { get; set; }
+
+  } //Class
+} //Namespace
