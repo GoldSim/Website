13a0aba4bde39746d5e502ceb61cc64491e3f981
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-03-25 17:28:39 -0700

    [Untested] Applied `TechnicalPaperListTopicViewModel`

--- Models/TechnicalPaperListTopicViewModel.cs
+++ Models/TechnicalPaperListTopicViewModel.cs
@@ -3,6 +3,7 @@
 | Client        Goldsim
 | Project       Website
 \=============================================================================================================================*/
+using Ignia.Topics.Mapping;
 using Ignia.Topics.ViewModels;
 
 namespace GoldSim.Web.Models {
@@ -14,6 +15,11 @@ namespace GoldSim.Web.Models {
   ///   Provides a strongly-typed data transfer object for feeding views with information about a Technical Paper List topic.
   /// </summary>
   public class TechnicalPaperListTopicViewModel : ContentListTopicViewModel {
+
+    [Metadata("FieldCategories")]
+    public TopicViewModelCollection<LookupListItemTopicViewModel> FieldCategories { get; set; }
+
+    public TopicViewModelCollection<TechnicalPaperTopicViewModel> TechnicalPapers { get; set; }
 
   } // Class
 
