9e7413b399ef59d02a2f4ae4586c8c1380323441
Katherine Trunkey <ktrunkey@gmail.com>
2018-03-26 16:01:13 -0700

    Established `ApplicationContainerTopicViewModel`

---
+++ Models/ApplicationContainerTopicViewModel.cs
@@ -0,0 +1,23 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Goldsim
+| Project       Website
+\=============================================================================================================================*/
+using Ignia.Topics.ViewModels;
+
+namespace GoldSim.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: APPLICATION CONTAINER TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a <c>ApplicationContainer</c>
+  ///   topic.
+  /// </summary>
+  public class ApplicationContainerTopicViewModel {
+
+    public TopicViewModelCollection<ApplicationPageTopicViewModel> Children { get; set; }
+
+  } // Class
+
+} // Namespace
