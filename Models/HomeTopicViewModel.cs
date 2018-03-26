e1f1e793317cb46752309fad2c487ebc34345649
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-03-25 23:08:19 -0700

    Established placeholder for `HomeTopicViewModel`
    Still need to establish an actual `Home` content type; the current one is left over from the legacy website.

---
+++ Models/HomeTopicViewModel.cs
@@ -0,0 +1,28 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ignia.Topics.Mapping;
+using Ignia.Topics.ViewModels;
+
+namespace GoldSim.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: HOME TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a <c>Home</c> topic.
+  /// </summary>
+  public class HomeTopicViewModel: ContentItemTopicViewModel {
+
+    [Recurse(Relationships.Children)]
+    public TopicViewModelCollection<PageTopicViewModel> Related { get; set; }
+
+  } //Class
+} //Namespace
