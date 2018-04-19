270b5915fd8f3308828edcae1fef4b05f0120bfb
Jeremy Caney <Jeremy.Caney@Ignia.com>
2018-04-19 15:08:24 -0700

    Implemented `ErrorControllerBase`
    The `ErrorController` logic has been centralized as part of the `ErrorControllerBase` class in `Ignia.Topics.Web.Mvc`. This class is generic to allow any view models to be used. Since MVC doesn't route effectively with generics, this class must be derived from, and supplied with the appropriate view model class (in GoldSim's case, the `PageTopicViewModel` works fine).

--- Controllers/ErrorController.cs
+++ Controllers/ErrorController.cs
@@ -7,6 +7,7 @@ using System.Web.Mvc;
 using Ignia.Topics;
 using Ignia.Topics.Repositories;
 using Ignia.Topics.ViewModels;
+using Ignia.Topics.Web.Mvc.Controllers;
 
 namespace GoldSim.Web.Controllers {
 
@@ -16,98 +17,7 @@ namespace GoldSim.Web.Controllers {
   /// <summary>
   ///   Provides access to the views associated with 400 and 500 error results.
   /// </summary>
-  public class ErrorController : Controller {
-
-    /*==========================================================================================================================
-    | GET: /Error/Error
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Provides the default custom error page for the site.
-    /// </summary>
-    /// <returns>The site's default error view.</returns>
-    public ActionResult Error() {
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Instantiate view model
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      var viewModel             = new PageTopicViewModel();
-      viewModel.Key             = "Error";
-      viewModel.UniqueKey       = "Error:Error";
-      viewModel.ContentType     = "Page";
-      viewModel.Title           = "General Error";
-      viewModel.MetaKeywords    = "";
-      viewModel.MetaDescription = "";
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Return the view
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      return View("Error", viewModel);
-
-    }
-
-    /*==========================================================================================================================
-    | GET: /Error/NotFound
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Provides the custom 404 error page for the site.
-    /// </summary>
-    /// <returns>The site's 404 (not found) error view.</returns>
-    public ActionResult NotFound() {
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Return the proper status code
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      Response.StatusCode       = 404;
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Instantiate view model
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      var viewModel             = new PageTopicViewModel();
-      viewModel.Key             = "NotFound";
-      viewModel.UniqueKey       = "Error:NotFound";
-      viewModel.ContentType     = "Page";
-      viewModel.Title           = "Page Not Found";
-      viewModel.MetaKeywords    = "";
-      viewModel.MetaDescription = "";
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Return the view
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      return View("NotFound", viewModel);
-
-    }
-
-    /*==========================================================================================================================
-    | GET: /Error/InternalServer
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Provides the custom 500 error page for the site.
-    /// </summary>
-    /// <returns>The site's 500 (internal server) error view.</returns>
-    public ActionResult InternalServer() {
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Return the proper status code
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      Response.StatusCode       = 500;
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Instantiate model
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      var viewModel             = new PageTopicViewModel();
-      viewModel.Key             = "InternalServer";
-      viewModel.UniqueKey       = "Error:InternalServer";
-      viewModel.ContentType     = "Page";
-      viewModel.Title           = "Internal Server Error";
-      viewModel.MetaKeywords    = "";
-      viewModel.MetaDescription = "";
-
-      /*------------------------------------------------------------------------------------------------------------------------
-      | Return the view
-      \-----------------------------------------------------------------------------------------------------------------------*/
-      return View("InternalServer", viewModel);
-
-    }
+  public class ErrorController : ErrorControllerBase<PageTopicViewModel> {
 
   } // Class
 
