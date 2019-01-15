4395101ec7af040971e647c1cf7f65ec3deac2d8
Katherine Trunkey <ktrunkey@gmail.com>
2019-01-15 14:08:02 -0800

    Braintree integration: `PaymentsTopicViewModel` stub

---
+++ Models/PaymentsTopicViewModel.cs
@@ -0,0 +1,21 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Ignia, LLC
+| Project       Topics Library
+\=============================================================================================================================*/
+using Ignia.Topics.ViewModels;
+
+namespace GoldSim.Web.Models {
+
+  /*============================================================================================================================
+  | VIEW MODEL: PAYMENTS TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about a <c>Payments</c> topic.
+  /// </summary>
+  public class PaymentsTopicViewModel : PageTopicViewModel {
+
+
+  } // Class
+
+} // Namespace
