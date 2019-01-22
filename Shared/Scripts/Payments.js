a076f2988f2687ab6d7f0114dad1259e609dd4df
Katherine Trunkey <ktrunkey@gmail.com>
2019-01-22 14:28:21 -0800

    Cleaned up formatting and added comments for readability

--- Shared/Scripts/Payments.js
+++ Shared/Scripts/Payments.js
@@ -6,63 +6,86 @@
 
 function executeBraintree(clientToken) {
 
-  var form = document.querySelector('#PaymentsForm');
-  var submit = document.querySelector('button[type="submit"]');
+  /**
+   * Establish variables
+   */
+  var
+    form        = document.querySelector('#PaymentsForm'),
+    submit      = document.querySelector('button[type="submit"]');
 
+  /**
+   * Initialize Braintree client instance
+   */
   var clientInstance = braintree.client.create({
     authorization: clientToken
-  }, function (clientError, clientInstance) {
+  },
+  function (clientError, clientInstance) {
 
+    /**
+      * Exit if there is an error creating the client instance
+      */
     if (clientError) {
       console.error(clientError);
       return;
     }
 
+    /**
+     * Initialize Braintree Hosted Fields configuration
+     */
     braintree.hostedFields.create({
       client: clientInstance,
       styles: {
         'input': {
-          'font-family': 'Helvetica, Roboto, Arial, sans-serif',
-          'font-size': '.9375rem',
-          'line-height': '1.5',
-          'color': 'rgb(32, 32, 32)'
+          'font-family'         : 'Helvetica, Roboto, Arial, sans-serif',
+          'font-size'           : '.9375rem',
+          'line-height'         : '1.5',
+          'color'               : 'rgb(32, 32, 32)'
         }
       },
       fields: {
         number: {
-          selector: '#CardNumber',
-          placeholder: '1111 1111 1111 1111'
+          selector              : '#CardNumber',
+          placeholder           : '1111 1111 1111 1111'
         },
         cvv: {
-          type: 'password',
-          selector: '#Cvv',
-          placeholder: '123',
-          maskInput: {
-            character: '*'
+          selector              : '#Cvv',
+          placeholder           : '123',
+          maskInput             : {
+            character           : '*'
           }
         },
         expirationMonth: {
-          selector: '#ExpirationMonth',
-          placeholder: '01'
+          selector              : '#ExpirationMonth',
+          placeholder           : '01'
         },
         expirationYear: {
-          selector: '#ExpirationYear',
-          placeholder: '2021'
+          selector              : '#ExpirationYear',
+          placeholder           : '2021'
         },
         postalCode: {
-          selector: '#PostalCode',
-          placeholder: '12345'
+          selector              : '#PostalCode',
+          placeholder           : '12345'
         }
       }
-    }, function (hostedFieldsError, hostedFieldsInstance) {
+    },
+    function (hostedFieldsError, hostedFieldsInstance) {
 
+      /**
+       * Exit if there is an error creating the Hosted Fields
+       */
       if (hostedFieldsError) {
         console.error(hostedFieldsError);
         return;
       }
 
+      /**
+       * Enable submit button once Hosted Fields are created
+       */
       submit.removeAttribute('disabled');
 
+      /**
+       * Handle form submission, including errors
+       */
       form.addEventListener('submit', function (event) {
         event.preventDefault();
 
@@ -72,16 +95,17 @@ function executeBraintree(clientToken) {
 
             switch (tokenizeError.code) {
 
+              // Occurs when ALL fields are empty
               case 'HOSTED_FIELDS_FIELDS_EMPTY':
-                // occurs when none of the fields are filled in
                 $('label.hosted-field-label.required').addClass('is-invalid-label');
                 $('span.hosted-field.required').addClass('is-invalid-input');
                 $('#EmptyFieldsError').removeClass('is-hidden');
                 $('#ClientSideErrors').removeClass('is-hidden');
                 break;
 
+              // Occurs when certain fields do not pass client side validation
               case 'HOSTED_FIELDS_FIELDS_INVALID':
-                // occurs when certain fields do not pass client side validation
+                // Expose client-side error messaging depending on the erring field(s)
                 $.each(tokenizeError.details.invalidFieldKeys, function (index, key) {
                   if (key === 'number') {
                     $('#CCNumberError').removeClass('is-hidden');
@@ -93,6 +117,7 @@ function executeBraintree(clientToken) {
                     $('#PostalCodeError').removeClass('is-hidden');
                   }
                 });
+                // Set error styles for erring fields and their labels
                 $.each(tokenizeError.details.invalidFields, function (fieldContainer, element) {
                   var containerElement = element.getAttribute('id');
                   $('label[for="' + containerElement + '"]').addClass('is-invalid-label');
@@ -103,13 +128,13 @@ function executeBraintree(clientToken) {
                 });
                 break;
 
+              // Occurs for any other tokenization error on the server
               case 'HOSTED_FIELDS_FAILED_TOKENIZATION':
-                // occurs for any other tokenization error on the server
                 console.error('Tokenization failed server side. Is the card valid?');
                 break;
 
+              // Occurs when the Braintree gateway cannot be contacted
               case 'HOSTED_FIELDS_TOKENIZATION_NETWORK_ERROR':
-                // occurs when the Braintree gateway cannot be contacted
                 console.error('Network error occurred when tokenizing.');
                 break;
 
@@ -117,13 +142,18 @@ function executeBraintree(clientToken) {
                 console.error(tokenizeError);
             }
 
+            // Expose the error messages
             $('#ClientSideErrors').removeClass('is-hidden');
 
             return;
           }
 
-          document.querySelector('#PaymentMethodNonce').value = payload.nonce;
+          // Set the payment nonce hidden field value
+          document.querySelector('#PaymentMethodNonce').value   = payload.nonce;
+
+          // Submit the form
           form.submit();
+
         });
       }, false);
 
