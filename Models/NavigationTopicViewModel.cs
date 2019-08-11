c84eeb8e78b4b9f8536d06134732b4380c96cb04
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-08-11 02:28:11 -0700

    Updated to `alpha.257` build of *OnTopic*
    Initially, the `DotNetStandard` branch was operating off of the `dotnetstandard0001` build, which was triggered directly from the `DotNetStandard` branch. That branch has since been merged with `Develop`, alongside some other ancillary changes, and is considered a release candidate for the *.NET Standard* version of *OnTopic*. This update ensures *GoldSim* is being tested against this latest build.

--- Models/NavigationTopicViewModel.cs
+++ Models/NavigationTopicViewModel.cs
@@ -6,6 +6,7 @@
 using System.Collections.ObjectModel;
 using System.ComponentModel;
 using Ignia.Topics;
+using Ignia.Topics.Models;
 using Ignia.Topics.ViewModels;
 
 namespace GoldSim.Web.Models {
