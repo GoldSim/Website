01100592cc758a8b81347af26db2a830ef86cb11
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-09-28 16:45:41 -0700

    Introduce `Startup` class
    In **ASP.NET Core** applications, the application is configured using a `Startup` class. This effectively replaces the `App_Start\RouteConfig` class used in **ASP.NET 2+ Framework** sites. The configuration is pretty much standard out-of-the-box defaults, with the exception of adding in support for **OnTopic** through e.g., `AddTopicSupport()`, `GoldSimActivator`, and `MapTopicRoute()`.

---
+++ Startup.cs
@@ -0,0 +1,124 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+using Ignia.Topics.AspNetCore.Mvc;
+
+namespace GoldSim.Web {
+
+  /*============================================================================================================================
+  | CLASS: STARTUP
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Configures the application and sets up dependencies.
+  /// </summary>
+  public class Startup {
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Constructs a new instances of the <see cref="Startup"/> class. Accepts an <see cref="IConfiguration"/>.
+    /// </summary>
+    /// <param name="configuration">
+    ///   The shared <see cref="IConfiguration"/> dependency.
+    /// </param>
+    public Startup(IConfiguration configuration) {
+      Configuration = configuration;
+    }
+
+    /*==========================================================================================================================
+    | PROPERTY: CONFIGURATION
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a (public) reference to the application's <see cref="IConfiguration"/> service.
+    /// </summary>
+    public IConfiguration Configuration { get; }
+
+    /*==========================================================================================================================
+    | METHOD: CONFIGURE SERVICES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides configuration of services. This method is called by the runtime to bootstrap the server configuration.
+    /// </summary>
+    public void ConfigureServices(IServiceCollection services) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: MVC
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      services.AddControllersWithViews()
+
+        //Add OnTopic support
+        .AddTopicSupport();
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Register: Activators
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var activator = new GoldSimActivator(Configuration.GetConnectionString("OnTopic"));
+
+      services.AddSingleton<IControllerActivator>(activator);
+
+    }
+
+    /*==========================================================================================================================
+    | METHOD: CONFIGURE (APPLICATION)
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides configuration the application. This method is called by the runtime to bootstrap the application
+    ///   configuration, including the HTTP pipeline.
+    /// </summary>
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: Error Pages
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (env.IsDevelopment()) {
+        app.UseDeveloperExceptionPage();
+      }
+      else {
+        app.UseExceptionHandler("/Error/500");
+        // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+        app.UseHsts();
+      }
+
+     /*------------------------------------------------------------------------------------------------------------------------
+     | Configure: Server defaults
+     \-----------------------------------------------------------------------------------------------------------------------*/
+      app.UseHttpsRedirection();
+      app.UseStaticFiles();
+      app.UseRouting();
+      app.UseCors("default");
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Configure: MVC
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      app.UseEndpoints(endpoints => {
+        endpoints.MapControllers();
+        endpoints.MapTopicRoute("Web");
+        endpoints.MapControllerRoute(
+          name: "LegacyRedirect",
+          pattern: "Page/{pageId}",
+          defaults: new { controller = "LegacyRedirect", action = "Redirect" }
+        );
+        endpoints.MapControllerRoute(
+          name: "default",
+          pattern: "{controller=Home}/{action=Index}/{id?}"
+        );
+      });
+
+    }
+
+  } //Class
+} //Namespace
