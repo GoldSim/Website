2bfbb5b78deebca859bf6a7e5c0d890f6986e6b8
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-09-29 14:12:17 -0700

    Reconfigured *Application Insights*
    **Application Insights** had a number of generated, undocumented files with errors. Instead of going through and debugging these to figure out which were relevant, I instead simply removed all of them and reconfigured **Application Insights** for **ASP.NET Core**. This, it turns out, is much cleaner than our previous implementation! This gets rid of `AiHandleErrorAttribute` (which we never really used), `FilterConfig`, and the troublesome `ApplicationInsights.config` (which would often end up with non-functional formatting changes after every upgrade). Good riddance! We welcome to `Microsoft.ApplicationInsights.AspNetCore` 4.8.0.

--- FilterConfig.cs
+++
@@ -1,9 +0,0 @@
-using System.Web.Mvc;
-
-namespace GoldSim.Web{
-  public class FilterConfig {
-    public static void RegisterGlobalFilters(GlobalFilterCollection filters) {
-      filters.Add(new ErrorHandler.AiHandleErrorAttribute());
-    }
-  }
-}
