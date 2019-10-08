6f3f34230cd3be2b91e7f1aa659350aca935d0d5
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-10-08 14:10:29 -0700

    Introduced `ISmtpService` abstraction
    The `ISmtpService` abstraction allows emails to be sent through an SMTP server that is configured and injected as part of the _Composition Root_. Instead of introducing its own model for composing email messages, it simply reuses the well-known out-of-the-box `MailMessage` that ships with **ASP.NET Standard**.

---
+++ Services/ISmtpService.cs
@@ -0,0 +1,37 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using Ignia.Topics;
+
+namespace GoldSim.Web.Services {
+
+  /*============================================================================================================================
+  | INTERFACE: SMTP SERVICE
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Given a <see cref="MailMessage", will send through an SMTP service configured as part of a concrete implementation.
+  /// </summary>
+  /// <remarks>
+  ///   For simplicity and familiarity, the <see cref="ISmtpService"/> uses ASP.NET Core's out-of-the-box <see
+  ///   cref="MailMessage"/> class as a well-known data transfer object, so that implementation-specific versions needn't be
+  ///   known to implementers, and to prevent the need to reinvent a model that is already built into the framework.
+  /// </remarks>
+  public interface ISmtpService {
+
+    /*==========================================================================================================================
+    | SEND
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Sends the <see cref="MailMessage"/>.
+    /// </summary>
+    Task SendAsync(MailMessage mailMessage);
+
+  } // Interface
+
+} // Namespace
