5793df7ff696e5566e0c5ea7994bd4243f5ec85d
Jeremy Caney <Jeremy.Caney@Ignia.com>
2019-10-09 12:00:30 -0700

    Establish `FormPageViewModel` as base class
    By separating out `FormPageViewModel` and `FormPageViewModel<T>` into separate classes, we're able to have common elements (such as a layout page) target all `FormPageViewModel`s, independent of their `T BindingModel`. This allows individual form pages to be strongly typed to a specific binding model, while still allowing higher-level pages to access properties of the `FormPageViewModel`, such as `SubmitButtonLabel` or `FollowUpPage`.

--- Models/FormPageTopicViewModel{T}.cs
+++ Models/FormPageTopicViewModel{T}.cs
@@ -14,7 +14,7 @@ namespace GoldSim.Web.Models {
   /// <summary>
   ///   A view model for rendering a form page with a strongly-typed binding model.
   /// </summary>
-  public class FormPageTopicViewModel<T>: PageTopicViewModel where T : class, new() {
+  public class FormPageTopicViewModel<T> : FormPageTopicViewModel where T : class, new() {
 
     /*==========================================================================================================================
     | CONSTRUCTOR
@@ -24,56 +24,8 @@ namespace GoldSim.Web.Models {
     /// </summary>
     /// <returns>A <see cref="FormPageTopicViewModel"/>.</returns>
     public FormPageTopicViewModel(T bindingModel = null) {
-      BindingModel = bindingModel?? new T();
+      BindingModel = bindingModel ?? new T();
     }
-
-    /*==========================================================================================================================
-    | FOLLOW-UP PAGE
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   References the <see cref="Topic.Id"/> of the page that should be redirected to upon completion.
-    /// </summary>
-    public int FollowUpPage { get; set; }
-
-    /*==========================================================================================================================
-    | SUBMIT BUTTON LABEL
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Defines the label to use for the submit button.
-    /// </summary>
-    public string SubmitButtonLabel { get; set; } = "Submit";
-
-    /*==========================================================================================================================
-    | DISABLE EMAIL RECEIPT
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Optionally overrides the email receipt that is sent by default.
-    /// </summary>
-    public bool DisableEmailReceipt { get; set; } = false;
-
-    /*==========================================================================================================================
-    | EMAIL SUBJECT
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Optionally overrides the subject of the email receipt.
-    /// </summary>
-    public string EmailReceipt { get; set; }
-
-    /*==========================================================================================================================
-    | EMAIL RECIPIENT
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Optionally overrides the recipient of the email receipt.
-    /// </summary>
-    public string EmailRecipient { get; set; }
-
-    /*==========================================================================================================================
-    | EMAIL SENDER
-    \-------------------------------------------------------------------------------------------------------------------------*/
-    /// <summary>
-    ///   Optionally overrides the sender of the email receipt.
-    /// </summary>
-    public string EmailSender { get; set; }
 
     /*==========================================================================================================================
     | BINDING MODEL
