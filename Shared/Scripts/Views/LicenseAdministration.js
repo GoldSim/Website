52f119721e94bb685a96013aa5ec753d84210567
JeremyCaney <jeremy.caney@ignia.com>
2019-11-21 15:56:21 -0800

    Introduced basic license processor interface
    Previously, while the `ReportingService` (now `LicenseExportService`) was handled via a controller (now `LicensesController`), the actual frontend interface was implemented via the legacy `LicenseRequests.aspx` **ASP.NET WebForms** page (ugh). This commit is an initial port of that over to **ASP.NET Core** using the **GoldSim** interface. (The previous implementation used the **OnTopic** interface, which looked nice, but doesn't make sense for a GoldSim-specific tool.) This required ripping out a lot of extranous markup required by **ASP.NET WebForms** and **Twitter Bootstrap** (which **OnTopic** uses). The CSS and JS were previously inline, and are now moved to separate `View` resources, which will be built as part of the new **Gulp** workflow, thus keeping the view a bit more focused on the markup. The class names and markup will still likely require a bit of care from @ktrunkey, but they are currently functional and looks more-or-less right.

---
+++ Shared/Scripts/Views/LicenseAdministration.js
@@ -0,0 +1,16 @@
+﻿$(function () {
+
+  /**
+    * Instantiates the dataTable plugin for the pending requests; $requestsTableRows finds all rows (even those hidden by
+    * paging) for the dataTable.
+    */
+  var $requestsTable = $('table[id*="PendingRequests"]').DataTable({
+    pageLength                  : 75,
+    //'paging'                  : false,
+    lengthMenu                  : [[10, 25, 50, -1], [10, 25, 50, "All"]],
+    order                       : [[4, 'asc']],
+    stateSave                   : false
+  });
+  var $requestsTableRows = $requestsTable.rows().nodes();
+
+});
