7ecec709d843ba3111c96e2d22160cb0c67451ab
JeremyCaney <jeremy.caney@ignia.com>
2019-11-21 15:57:44 -0800

    Reintroduced `Delete()` functionality as a controller action
    Introduced the ablity to select multiple topics and delete them by posting the form back to a `Delete()` action. Includes some **jQuery** for handling bulk select use cases.

--- Shared/Scripts/Views/LicenseAdministration.js
+++ Shared/Scripts/Views/LicenseAdministration.js
@@ -13,4 +13,44 @@
   });
   var $requestsTableRows = $requestsTable.rows().nodes();
 
+  /**
+    * Finds all rows in the table and selects all checkboxes, depending on state of table header checkbox; also sets/resets
+    * the hidden field value of selected/checked requests
+    */
+  $('#SelectAllRecords').change(function () {
+
+    if (!$(this).is(':checked')) {
+      $('input[type="checkbox"]', $requestsTableRows).prop('checked', false);
+      $(this).removeClass('all-selected');
+    }
+    else {
+      $('input[type="checkbox"]', $requestsTableRows).prop('checked', true);
+      $(this).addClass('all-selected');
+    }
+
+  });
+
+  /**
+    * De-checks the "select all" checkbox in the event an individual row checkbox is toggled; also sets/resets the hidden
+    * field value of selected/checked requests
+    */
+  $('.Request-Row td input[type="checkbox"]').change(function () {
+
+    var $selectAllCheckbox = $('#SelectAllRecords');
+
+    // Update "select all" checkbox
+    if ($($selectAllCheckbox).hasClass('all-selected')) {
+      $($selectAllCheckbox).prop('checked', false);
+    }
+    $($selectAllCheckbox).toggleClass('all-selected');
+
+  });
+
 });
+
+/**
+  * Provide a warning and confirmation when user chooses to delete a Topic
+  */
+function confirmDelete() {
+  return confirm('Are you sure you want to delete the selected license requests? This action cannot be undone.');
+}
