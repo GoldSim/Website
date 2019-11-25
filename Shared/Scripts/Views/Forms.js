ae6d97fc12ec9824ecf7f0e6378e9426031cfa10
JeremyCaney <jeremy.caney@ignia.com>
2019-11-25 13:49:59 -0800

    Introduced `Forms.js` view script with view logic
    Moved the range extension from the `_Forms.cshtml` layout into a new `Forms.js` view script. Reintroduced the script to treat the Radionuclide and Contaminant transport modules are exclusive, such that only one can be selected at a time. Added the view script to the `_Forms.cshtml`.

---
+++ Shared/Scripts/Views/Forms.js
@@ -0,0 +1,31 @@
+﻿$(function () {
+
+  /**
+    * Treate Radionuclide and Contaminant transport modules as exclusive selections
+    */
+  var rtCheckbox = $("#BindingModel_Modules_RadionuclideTransport");
+  var ctCheckbox = $("#BindingModel_Modules_ContaminantTransport");
+
+  rtCheckbox.on('change', function(e) {
+    ctCheckbox.prop('checked', false);
+  });
+  ctCheckbox.on('change', function(e) {
+    rtCheckbox.prop('checked', false);
+  });
+
+  /**
+    * Extend the range validator return a boolean
+    */
+  //
+  var defaultRangeValidator = $.validator.methods.range;
+  $.validator.methods.range = function (value, element, param) {
+    if (element.type === 'checkbox') {
+      //If it's a checkbox, return its checked state
+      return element.checked;
+    } else {
+      //Otherwise, run the default validator
+      return defaultRangeValidator.call(this, value, element, param);
+    }
+  };
+
+});
