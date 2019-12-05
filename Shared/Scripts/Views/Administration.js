eced0cc5338230f6ed52f78bfcde43dbdf69c46d
JeremyCaney <jeremy.caney@ignia.com>
2019-12-05 13:04:22 -0800

    Centralized and generalized administration scripts, styles
    Renamed `LicenseAdministration.(js|css)` to simply `Administration.(js|css)` and generalized the language and identifiers to work across multiple administration pages. This makes these scripts better suited to serve both `Licenses/LicenseAdministration.cshtml` as well as `Invoices/Index.cshtml`. As part of this, needed to change the script locations as well as some class names in the views themselves, and decentralize the initialization of `DataTable()` since it contains table-specific properties (such as `lengthMenu` and `order`) which are harder to generalize.

---
+++ Shared/Scripts/Views/Administration.js
@@ -0,0 +1,45 @@
+﻿$(function() {
+
+  /**
+    * Finds all rows in the table and selects all checkboxes, depending on state of table header checkbox; also sets/resets
+    * the hidden field value of selected/checked records
+    */
+  $('#SelectAllRecords').change(function() {
+
+    if (!$(this).is(':checked')) {
+      $('tr.record input[type="checkbox"]').prop('checked', false);
+      $(this).removeClass('all-selected');
+    }
+    else {
+      $('tr.record input[type="checkbox"]').prop('checked', true);
+      $(this).addClass('all-selected');
+    }
+
+  });
+
+  /**
+    * De-checks the "select all" checkbox in the event an individual row checkbox is toggled; also sets/resets the hidden
+    * field value of selected/checked records.
+    */
+  $('tr.record td input[type="checkbox"]').change(function() {
+
+    var $selectAllCheckbox = $('#SelectAllRecords');
+
+    // Update "select all" checkbox
+    if ($($selectAllCheckbox).hasClass('all-selected')) {
+      $($selectAllCheckbox).prop('checked', false);
+    }
+    $($selectAllCheckbox).toggleClass('all-selected');
+
+  });
+
+});
+})(jQuery);
+
+/**
+  * Provide a warning and confirmation when user chooses to delete a Topic
+  * @return {object} Confirmation prompt confirming the user's selection
+  */
+function confirmDelete() {
+  return confirm('Are you sure you want to delete the selected records? This action cannot be undone.');
+}
