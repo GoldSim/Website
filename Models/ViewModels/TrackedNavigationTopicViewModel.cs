ec2ebb047a8b146906c4f1998070bed17180b0f1
JeremyCaney <jeremy.caney@ignia.com>
2020-06-19 16:02:36 -0700

    Introduced `TrackedNavigationTopicViewModel`
    The `TrackedNavigationTopicViewModel` provides a class for handling navigation which include progress tracking—i.e., which will be flagged based on whether a user has visited a page or completed a section. This is necessary to support GoldSim's courseware, which will use (initially) cookies to track the user's state.

---
+++ Models/ViewModels/TrackedNavigationTopicViewModel.cs
@@ -0,0 +1,30 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Goldsim
+| Project       Website
+\=============================================================================================================================*/
+using System.Collections.ObjectModel;
+using OnTopic.Models;
+using OnTopic.ViewModels;
+
+namespace GoldSim.Web.Models.ViewModels {
+
+  /*============================================================================================================================
+  | VIEW MODEL: TRACKED NAVIGATION TOPIC
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for feeding views with information about the navigation alongside
+  ///   progress tracking information.
+  /// </summary>
+  /// <remarks>
+  ///   No topics are expected to have a <c>Navigation</c> content type. Instead, this view model is expected to be manually
+  ///   constructed by the <see cref="LayoutController"/>.
+  /// </remarks>
+  public class TrackedNavigationTopicViewModel: PageTopicViewModel, INavigationTopicViewModel<TrackedNavigationTopicViewModel> {
+
+    public bool? IsVisited { get; set; } = null;
+    public Collection<TrackedNavigationTopicViewModel> Children { get; } = new Collection<TrackedNavigationTopicViewModel>();
+    public bool IsSelected(string uniqueKey) => $"{uniqueKey}:"?.StartsWith($"{UniqueKey}:") ?? false;
+
+  } // Class
+} // Namespace
