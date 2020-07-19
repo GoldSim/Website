1ec1c5a08e3947d1f7bf38e2b05513a02d970283
JeremyCaney <jeremy.caney@ignia.com>
2020-07-19 16:40:16 -0700

    Moved MPEG-DASH player configuration to a new `Home.js`
    Normally, when an individual view needs its own JavaScript, we create a view-specific file in `/Shared/Scripts/Views`, and call it from the view template. That didn't happen for the homepage, in part because the scripts were so small, but also because there's only one implementation of the page, and thus less caching benefit to sharing the files across template instances.
    
    That said, as the script has grown, it makes more sense to centralize it, not only for consistency, but also to allow it to be subjected to JSLint, Uglify, &c. as part of our build script. This adds an external call, but also reduces the size of the download. And since the video doesn't need to play back by default, the extra HTTP call isn't a big deal. In fact, I've `defer`red the script since it doesn't need to be immediately available on page load.

---
+++ Shared/Scripts/Views/Home.js
@@ -0,0 +1,123 @@
+﻿/**
+ * (GOLDSIM WEB) HOME PAGE SCRIPTS
+ * @file Provides functions that are unique to the homepage.
+ * @namespace goldSimWeb
+ */
+$(function() {
+
+  /**
+    * Establish variables
+    */
+  var
+    isVideoLoaded           = false,
+    introductionPanelPosition = ($('#Introduction').offset().top - 24),
+    isSafari                = !!navigator.userAgent.match(/Version\/[\d\.]+.*Safari/),
+    isIos                   = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream,
+    isIe11                  = !!navigator.userAgent.match(/Trident\/7\./);
+
+  /**
+    * Handle splash screen arrow scroll click
+    */
+  $('img.arrow.scroll').click(function() {
+    $('html,body').animate({
+      scrollTop: introductionPanelPosition
+    }, 1000);
+  });
+
+  /**
+    * Instantiate carousel for large and small screens
+    */
+  $('.owl-carousel.large').owlCarousel({
+    items: 1,
+    margin: 0,
+    autoHeight: true,
+    nav: true,
+    navText: [
+      '<i class="fa fa-caret-left"></i>',
+      '<i class="fa fa-caret-right"></i>'
+    ],
+    loop: false
+  });
+  $('.owl-carousel.small').owlCarousel({
+    items: 1,
+    margin: 0,
+    autoHeight: false,
+    nav: true,
+    navText: [
+      '<i class="fa fa-caret-left"></i>',
+      '<i class="fa fa-caret-right"></i>'
+    ],
+    loop: false
+  });
+
+  /**
+    * Handle toggling of carousel slide text / screenshot
+    */
+  $.fn.toggleText = function (textOriginal, textToggled) {
+    if (this.text() == textOriginal) {
+      this.text(textToggled);
+    }
+    else {
+      this.text(textOriginal);
+    }
+    return this;
+  };
+  $('a.js-toggle-slide').click(function () {
+    $(this).siblings('.description, .screenshot').toggleClass('is-hidden');
+    $(this).toggleText('View Screenshot', 'View Description');
+  });
+
+  /**
+    * Handle video playback based on Foundation Reveal events
+    */
+  $(document).on('open.zf.reveal', '[data-reveal]', function () {
+    if (isVideoLoaded === false) {
+      var
+        manifestUrl         = 'https://media.GoldSim.com/Videos/Home/GoldSim_Overview_dash.mpd',
+        fallbackUrl         = 'https://31cac97e830ee523d21d-3991774b1862aed7fa3658c502b53d27.ssl.cf1.rackcdn.com/GoldSimm_Overview_082718.mp4',
+        dashPlayer          = dashjs.MediaPlayer().create();
+
+      //Configure dashPlayer
+      dashPlayer.updateSettings({
+        'streaming'         : {
+          'retryAttemps'    : {
+            'MPD'           : 2,
+            'XLinkExpansion': 2,
+            'IndexSegment'  : 3,
+            'InitializationSegment': 3,
+            'BitstreamSwitchingSegment': 3
+          },
+          'abr'             : {
+            'autoSwitchBitrate': {
+              'audio'       : false,
+              'video'       : true
+            }
+          }
+        }
+      });
+
+      // Only initialize DASH for supported browsers
+      if (!(isIos && isSafari) && !isIe11) {
+        dashPlayer.initialize(document.querySelector('#IntroductionVideo'), manifestUrl, true);
+      }
+      // Otherwise, use fallback / original video
+      else {
+        $('#IntroductionVideo source').attr('src', fallbackUrl);
+        $('#IntroductionVideo')[0].play();
+      }
+
+      // Toggle video loaded
+      isVideoLoaded = true;
+
+      // Track video play event
+      ga('send', 'event', 'Video', 'Play', 'Hompage Splash Video');
+    }
+    else {
+      $('#IntroductionVideo')[0].play();
+    }
+  });
+  $(document).on('closed.zf.reveal', '[data-reveal]', function () {
+    $('#IntroductionVideo')[0].pause();
+  });
+
+});
