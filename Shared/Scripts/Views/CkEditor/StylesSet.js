0c2928c01eb7f95cfd8018ed7fbaa7ee8590a070
JeremyCaney <jeremy.caney@ignia.com>
2020-07-21 15:01:46 -0700

    Added GoldSim specific styles to CkEditor
    In a previous commit (ccb462d), we had established a barebones `StyleSet.js` for configuring the OnTopic Editor with a set of custom styles. We hadn't, however, added any custom styles to it. This update resolves that, by adding common styles used across GoldSim content pages. Not all styles are included, but the ones used most frequently are.

--- Shared/Scripts/Views/CkEditor/StylesSet.js
+++ Shared/Scripts/Views/CkEditor/StylesSet.js
@@ -20,17 +20,84 @@ CKEDITOR.stylesSet.add( 'OnTopicStyleSet', [
   | FORMAT STYLES
   >-----------------------------------------------------------------------------------------------------------------------------
   | These are normally found in the format dropdown box. By reproducing them here, we can optionally remove the format dropdown.
+  >-----------------------------------------------------------------------------------------------------------------------------
+  { name                        : 'Heading 1',
+    element                     : 'h1'
+  },
+  { name                        : 'Heading 2',
+    element                     : 'h2'
+  },
+  { name                        : 'Heading 3',
+    element                     : 'h3'
+  },
+  { name                        : 'Body',
+    element                     : 'p'
+  },
+  {
+    name                        : 'Preformatted Text',
+    element                     : 'pre'
+  },
   \---------------------------------------------------------------------------------------------------------------------------*/
-  { name                : 'Heading 2',
-    element             : 'h2'
-  },
-  { name                : 'Heading 3',
-    element             : 'h3'
-  },
-  { name                : 'Body',
-    element             : 'p'
-  },
-  { name: 'Preformatted Text', element: 'pre' }
 
+  /*----------------------------------------------------------------------------------------------------------------------------
+  | CUSTOM INLINE STYLES
+  >-----------------------------------------------------------------------------------------------------------------------------
+  | Styles that are specific to GoldSim's stylesheet, and frequently used across pages
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  {
+    name                        : 'Figure',
+    element                     : 'figure',
+    attributes                  : {
+      'class'                   : 'illustration full'
+    }
+  },
+  {
+    name                        : 'Figure w/out Border',
+    element                     : 'figure',
+    attributes                  : {
+      'class'                   : 'illustration full'
+    }
+  },
+  {
+    name                        : 'Footnote',
+    element                     : 'p',
+    attributes                  : {
+      'class'                   : 'footnote'
+    }
+  },
+
+  /*----------------------------------------------------------------------------------------------------------------------------
+  | CUSTOM OBJECT STYLES
+  >-----------------------------------------------------------------------------------------------------------------------------
+  | Object styles require that the element be added to the page first. Once that's done, they
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  {
+    name                        : 'Callout Quote',
+    element                     : 'blockquote',
+    attributes                  : {
+      'class'                   : 'pull quote right'
+    }
+  },
+  {
+    name                        : 'Button',
+    element                     : 'a',
+    attributes                  : {
+      'class'                   : 'button primary large'
+    }
+  },
+  {
+    name                        : 'Callout (Picture)',
+    element                     : 'picture',
+    attributes                  : {
+      'class'                   : 'callout picture'
+    }
+  },
+  {
+    name                        : 'Callout (Thumbnail)',
+    element                     : 'picture',
+    attributes                  : {
+      'class'                   : 'callout picture thumbnail small'
+    }
+  }
 
 ]);
