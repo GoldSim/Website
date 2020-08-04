770cba414b9fd070ad68d466ff1ebcb3fa1c5766
JeremyCaney <jeremy.caney@ignia.com>
2020-08-04 12:37:19 -0700

    A naive implementation of a custom Foundation build
    Preferably, a custom Foundation build would operate off of `import`s of the ES6 source files, which would also allow for tree shaking to get rid of unnecessary code. Since we don't (yet) have an ES6 transpiler (such as Babel) configured, this isn't possible. As such, the naive version is just to incorporate the previously transpiled distribution plugins into a single script.
    
    This isn't ideal. All together, these partials end up with a lot of repetitive code, and so combining them all together is larger than the full distribution from Foundation. That said, by eliminating the pieces we don't need, it still ends up being smaller in the end. But it would be far smaller still if we could do our own transpilation using `import`s of the original ES6 modules.

---
+++ Shared/Scripts/Foundation.js
@@ -0,0 +1,46 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+
+/**
+ * GOLDSIM COMPOSITE SCRIPT
+ * @file Consolidates multiple partial scripts into one composite view.
+ * @namespace goldSimWeb
+ */
+
+/*==============================================================================================================================
+| IMPORTS
+\=============================================================================================================================*/
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.core.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.box.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.imageLoader.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.keyboard.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.mediaQuery.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.motion.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.nest.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.timer.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.touch.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.util.triggers.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.abide.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.accordion.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.accordionMenu.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.drilldown.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.dropdown.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.dropdownMenu.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.equalizer.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.interchange.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.magellan.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.offcanvas.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.orbit.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.responsiveMenu.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.responsiveToggle.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.reveal.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.slider.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.smoothScroll.js';
+import '../../node_modules/foundation-sites/dist/js/plugins/foundation.sticky.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.tabs.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.toggler.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.tooltip.js';
+//import '../../node_modules/foundation-sites/dist/js/plugins/foundation.responsiveAccordionTabs.js';
