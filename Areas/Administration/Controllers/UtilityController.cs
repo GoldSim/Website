25933b35233be76a83838f42826149352394ab36
JeremyCaney <jeremy.caney@ignia.com>
2020-08-11 12:26:05 -0700

    Established scaffolding for a new `UtilityController`
    The `UtilityController` is meant to house basic maintenance scripts used on GoldSim for administrative purposes. It requires authorization via AAD to utilize. It will house scripts for cleaning up data or running basic reports against OnTopic.

---
+++ Areas/Administration/Controllers/UtilityController.cs
@@ -0,0 +1,53 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using OnTopic;
+using OnTopic.Collections;
+using OnTopic.Querying;
+using OnTopic.Repositories;
+
+namespace GoldSim.Web.Administration.Controllers {
+
+  /*============================================================================================================================
+  | CLASS: REPORT CONTROLLER
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Allows GoldSim to report on embedded images and links.
+  /// </summary>
+  [Authorize]
+  [Area("Administration")]
+  public class UtilityController: Controller {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private readonly            ITopicRepository                _topicRepository;
+    private readonly            IWebHostEnvironment             _hostingEnvironment;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a <see cref ="UtilityController"/> with necessary dependencies.
+    /// </summary>
+    /// <returns>A topic controller for loading OnTopic views.</returns>
+    public UtilityController(
+      ITopicRepository topicRepository,
+      IWebHostEnvironment hostingEnvironment
+    ) {
+      _topicRepository          = topicRepository;
+      _hostingEnvironment       = hostingEnvironment;
+    }
+
+
+  } // Class
+} // Namespace
