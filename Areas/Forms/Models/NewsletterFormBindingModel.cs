a23f7f027e49e8f309c3e0288fe4ee7fe5f66a6d
JeremyCaney <jeremy.caney@ignia.com>
2020-09-03 09:24:40 -0700

    Add `Country` property to `NewsletterFormBindingModel`
    Technically, an alternative would have been to simply derive from `ExtendedContact`, but that would have additionally introduced `PhoneNumber`, which we don't need. Given that, I'm duplicating this definition.

--- Areas/Forms/Models/NewsletterFormBindingModel.cs
+++ Areas/Forms/Models/NewsletterFormBindingModel.cs
@@ -3,7 +3,9 @@
 | Client        Goldsim
 | Project       Website
 \=============================================================================================================================*/
+using System.ComponentModel.DataAnnotations;
 using GoldSim.Web.Forms.Models.Partials;
+using OnTopic.Mapping.Annotations;
 
 namespace GoldSim.Web.Forms.Models {
 
@@ -14,6 +16,17 @@ namespace GoldSim.Web.Forms.Models {
   ///   Provides a strongly-typed binding model representing the instructor version of the newsletter signup form.
   /// </summary>
   public class NewsletterFormBindingModel : CoreContact {
+
+    /*==========================================================================================================================
+    | PROPERTY: COUNTRY
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the country name.
+    /// </summary>
+    [Required]
+    [StringLength(75)]
+    [Metadata("Country")]
+    public virtual string Country { get; set; } = "United States of America";
 
   } //Class
 } //Namespace
