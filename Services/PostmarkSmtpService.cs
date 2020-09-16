9dc6961e711f263a6a5b2c3335ca4121cc7f6b06
JeremyCaney <jeremy.caney@ignia.com>
2020-09-15 18:19:00 -0700

    Introduced new `PostmarkSmtpService`
    We've been having issues with SendGrid's shared IP addresses getting blocked by spam filters. To mitigate that, we've signed up for Postmark. The `PostmarkSmtpService` allows us to plugin support for Postmark without updating any of the form code itself.

---
+++ Services/PostmarkSmtpService.cs
@@ -0,0 +1,72 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+using PostmarkDotNet;
+
+namespace GoldSim.Web.Services {
+
+  /*============================================================================================================================
+  | CLASS: POSTMARK (SMTP SERVICE)
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Given a <see cref="MailMessage", will send through the SendGrid SMTP service.
+  /// </summary>
+  public class PostmarkSmtpService: ISmtpService {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private readonly            PostmarkClient                  _smptClient;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="SendGridSmtpService"/> with necessary dependencies.
+    /// </summary>
+    /// <returns>A new instance of the <see cref="SendGridSmtpService"/>.</returns>
+    public PostmarkSmtpService(PostmarkClient postmarkClient) {
+      _smptClient = postmarkClient?? throw new ArgumentNullException(nameof(postmarkClient));
+    }
+
+    /*==========================================================================================================================
+    | SEND
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Sends the <see cref="MailMessage"/> via SendGrid.
+    /// </summary>
+    public async Task SendAsync(MailMessage mailMessage) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Assemble email
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      PostmarkMessage message   = new PostmarkMessage {
+        To                      = String.Join(", ", mailMessage.To.Select(x => x.Address)),
+        From                    = mailMessage.From.Address,
+        Subject                 = mailMessage.Subject,
+        HtmlBody                = mailMessage.Body,
+        TrackOpens              = true
+      };
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Send email
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var response              = await _smptClient.SendMessageAsync(message);
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate response
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (response.Status != PostmarkStatus.Success) {
+        throw new SmtpException(response.Message);
+      }
+
+    }
+
+  } // Class
+} // Namespace
