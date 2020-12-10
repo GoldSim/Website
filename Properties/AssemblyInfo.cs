a7f491e5e4f17fbd361bb55c6ea5fc3a2933cddc
JeremyCaney <jeremy.caney@ignia.com>
2020-12-10 15:40:34 -0800

    Allow `Get{Item}()` methods
    Suppress CA1024. These conventions are baked into OnTopic for View Components, and are otherwise deliberate for cases where a property wouldn't be appropriate.

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -4,6 +4,7 @@
 | Project       Topics Library
 \=============================================================================================================================*/
 using System;
+using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
 
 /*==============================================================================================================================
@@ -14,3 +15,10 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
 [assembly: Guid("70A25A53-63AF-42CA-87AB-E8358600E7F7")]
+
+/*==============================================================================================================================
+| DEFINE SUPPRESSIONS
+>===============================================================================================================================
+| Declare suppressions to code analysis rules that should be applied.
+\-----------------------------------------------------------------------------------------------------------------------------*/
+[assembly: SuppressMessage("Design", "CA1024:Use properties where appropriate", Justification = "Intentional design")]
