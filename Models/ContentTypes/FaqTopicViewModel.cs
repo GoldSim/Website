5fed2bcb6e39fae8caa109eac315dcef9b5962e9
JeremyCaney <Jeremy.Caney@Ignia.com>
2022-06-26 23:11:42 -0700

    Added `AttributeDictionary` constructor for `FaqTopicViewModel`
    This potentially improves performance of the `TopicMappingService` by allowing the view model to set its own properties instead of relying exclusively on reflection.

--- Models/ContentTypes/FaqTopicViewModel.cs
+++ Models/ContentTypes/FaqTopicViewModel.cs
@@ -14,5 +14,19 @@ namespace GoldSim.Web.Models.ContentTypes {
   /// </summary>
   public record FaqTopicViewModel: ContentListTopicViewModel {
 
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new <see cref="FaqTopicViewModel"/> with an <paramref name="attributes"/> dictionary.
+    /// </summary>
+    /// <param name="attributes">An <see cref="AttributeDictionary"/> of attribute values.</param>
+    public FaqTopicViewModel(AttributeDictionary attributes) : base(attributes) { }
+
+    /// <summary>
+    ///   Initializes a new <see cref="FaqTopicViewModel"/> with no parameters.
+    /// </summary>
+    public FaqTopicViewModel() { }
+
   } // Class
 } // Namespace
