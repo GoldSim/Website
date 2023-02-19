01d95d09146de36ff836529d1f32ed22191dedec
JeremyCaney <Jeremy.Caney@Ignia.com>
2023-02-19 13:19:57 -0800

    Introduce `OnActionExecuting` event for validating course access
    The new `OnActionExecuting` event handler evaluates whether the current course is marked as `IsPreview` and, if it is, necessitates that the current user be authenticated.

--- Areas/Courses/Controllers/CoursesController.cs
+++ Areas/Courses/Controllers/CoursesController.cs
@@ -3,6 +3,7 @@
 | Client        GoldSim
 | Project       Website
 \=============================================================================================================================*/
+using Microsoft.AspNetCore.Mvc.Filters;
 using OnTopic.AspNetCore.Mvc;
 using OnTopic.AspNetCore.Mvc.Controllers;
 using OnTopic.Mapping;
@@ -52,6 +53,41 @@ namespace GoldSim.Web.Courses.Controllers {
       | Call base logic
       \-----------------------------------------------------------------------------------------------------------------------*/
       return await base.IndexAsync(path).ConfigureAwait(true);
+
+    }
+
+    /*==========================================================================================================================
+    | EVENT HANDLER: ON ACTION EXECUTING
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Validates whether or not a given page should be displayed, based on the <code>IsPreview</code> attribute.
+    /// </summary>
+    public override void OnActionExecuting(ActionExecutingContext context) {
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Validate parameters
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      Contract.Requires(context, nameof(context));
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Determine if user is authenticated
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      var isAuthenticated       = context.HttpContext.User.Identity.IsAuthenticated;
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Handle preview areas
+      >-------------------------------------------------------------------------------------------------------------------------
+      | If a course is marked as preview, restrict access to authenticated users.
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      if (CurrentTopic.Attributes.GetBoolean("IsPreview", false, true) && !isAuthenticated) {
+        context.Result = new StatusCodeResult(401);
+        return;
+      }
+
+      /*------------------------------------------------------------------------------------------------------------------------
+      | Base processing
+      \-----------------------------------------------------------------------------------------------------------------------*/
+      base.OnActionExecuting(context);
 
     }
 
