6327ce6e80e6264cac60ffb15516b77fd693e9fa
JeremyCaney <Jeremy.Caney@Ignia.com>
2024-01-05 20:57:56 -0800

    Established a new `TopicSearchViewModel`
    This will store both the `Query` and the `Results` of the search. I've placed this in a new `GoldSim.Web.Models.Controllers` namespace, which can be used for view models associated with controllers, similar to the `GoldSim.Web.Models.Components` namespace, which is used for view models associated with components. I've added this new namespace to the `_ViewImports.cshtml` file, so all views are aware of it.

---
+++ Models/Controllers/TopicSearchViewModel.cs
@@ -0,0 +1,37 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Goldsim
+| Project       Website
+\=============================================================================================================================*/
+using System.Collections.ObjectModel;
+using GoldSim.Web.Controllers;
+using GoldSim.Web.Models.Associations;
+
+namespace GoldSim.Web.Models.Controllers {
+
+  /*============================================================================================================================
+  | VIEW MODEL: TOPIC SEARCH
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for values associated with the <see cref="TopicSearchController"/>.
+  /// </summary>
+  public record TopicSearchViewModel: PageTopicViewModel {
+
+    /*==========================================================================================================================
+    | QUERY
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides the requested query, so it can be reapplied to the search box.
+    /// </summary>
+    public string Query{ get; init; }
+
+    /*==========================================================================================================================
+    | RESULTS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides a list of results from the query.
+    /// </summary>
+    public ReadOnlyCollection<AssociatedTopicViewModel> Results { get; init; }
+
+  } // Class
+} // Namespace
