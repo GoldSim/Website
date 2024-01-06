87c173f1e3a3c9f0aa2ed7524affcd07986001c2
JeremyCaney <Jeremy.Caney@Ignia.com>
2024-01-05 20:45:53 -0800

    Introduced scaffolding for a `TopicSearchController`
    The `TopicSearchController` will enable a basic search page that allows topics to be searched.

---
+++ Controllers/TopicSearchController.cs
@@ -0,0 +1,49 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        GoldSim
+| Project       Website
+\=============================================================================================================================*/
+using System.Text.RegularExpressions;
+using GoldSim.Web.Models.Associations;
+using GoldSim.Web.Models.Controllers;
+using OnTopic;
+
+namespace GoldSim.Web.Controllers {
+
+  /*============================================================================================================================
+  | CLASS: TOPIC SEARCH CONTROLLER
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Allows searching for topics containing particular search patterns based on Regular Expressions.
+  /// </summary>
+  public class TopicSearchController : Controller {
+
+    /*==========================================================================================================================
+    | PRIVATE VARIABLES
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    private readonly            ITopicRepository                _topicRepository;
+
+    /*==========================================================================================================================
+    | CONSTRUCTOR
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Initializes a new instance of a Topic Search Controller with necessary dependencies.
+    /// </summary>
+    /// <returns>A topic search controller for loading OnTopic results.</returns>
+    public TopicSearchController(ITopicRepository topicRepository) : base() {
+      _topicRepository          = topicRepository;
+    }
+
+    /*==========================================================================================================================
+    | INDEX
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Searches all topics in the supplied <see cref="_topicRepository"/> for the <paramref name="query"/>, if provided.
+    /// </summary>
+    [HttpGet, HttpPost]
+    public IActionResult Index(string? query = null) {
+
+    }
+
+  } // Class
+} // Namespace
