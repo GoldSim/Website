dfbe0bd2a48a8c6ca7e8b4e2ac0f832fa0d8ddf6
JeremyCaney <Jeremy.Caney@Ignia.com>
2024-01-05 20:55:53 -0800

    Introduced a `FindTopics()` function to search topic tree
    The private `FindTopics()` function will recursively traverse the topics tree and search for topics containing attributes matching the search criteria—including those matching a regular expression—and add them to a list of `AssociatedTopicViewModel`s. I've also added this to the `Index()` function, which will initiate the recursive call.

--- Controllers/TopicSearchController.cs
+++ Controllers/TopicSearchController.cs
@@ -43,6 +43,53 @@ namespace GoldSim.Web.Controllers {
     [HttpGet, HttpPost]
     public IActionResult Index(string? query = null) {
 
+      /*-------------------------------------------------------------------------------------------------------------------------
+      | Find topics
+      \------------------------------------------------------------------------------------------------------------------------*/
+      var results = new List<AssociatedTopicViewModel>();
+
+      /*-------------------------------------------------------------------------------------------------------------------------
+      | Find the topic with the correct PageID.
+      \------------------------------------------------------------------------------------------------------------------------*/
+      if (!String.IsNullOrWhiteSpace(query)) {
+        FindTopics(_topicRepository.Load(), query, results);
+      }
+
+
+    }
+
+    /*==========================================================================================================================
+    | FIND TOPICS
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Recursively searches all topics in the <paramref name="topic"/> tree for the supplied <paramref name="query"/> and
+    ///   adds any results to the <paramref name="results"/> set.
+    /// </summary>
+    /// <param name="topic">The <see cref="Topic"/> to search within.</param>
+    /// <param name="query">The search term to look for in each attribute.</param>
+    /// <param name="results">The collection of positive matches.</param>
+    [HttpGet]
+    private static void FindTopics(Topic topic, string query, List<AssociatedTopicViewModel> results) {
+
+      // Search each attribute for the given query
+      foreach (var attribute in topic.Attributes) {
+        if (Regex.IsMatch(attribute.Value, query, RegexOptions.Compiled | RegexOptions.IgnoreCase)) {
+          results.Add(
+            new() {
+               Title            = topic.Title?? topic.Key,
+               ShortTitle       = topic.Title?? topic.Key,
+               WebPath          = topic.GetWebPath()
+            }
+          );
+          break;
+        }
+      }
+
+      // Recursively search each child topic
+      foreach (var childTopic in topic.Children) {
+        FindTopics(childTopic, query, results);
+      }
+
     }
 
   } // Class
