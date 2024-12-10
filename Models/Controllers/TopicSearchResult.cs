570ff6ba5b50fb467c4284e151d1ecf5f1ae42d8
JeremyCaney <Jeremy.Caney@Ignia.com>
2024-12-09 18:13:00 -0800

    Introduce a new `TopicSearchResult` view model
    This will be used to track the specific matches found by the query—and, if available, what the replacement result will be.

---
+++ Models/Controllers/TopicSearchResult.cs
@@ -0,0 +1,46 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Goldsim
+| Project       Website
+\=============================================================================================================================*/
+using System.Collections.ObjectModel;
+using GoldSim.Web.Controllers;
+using GoldSim.Web.Models.Associations;
+
+namespace GoldSim.Web.Models.Controllers {
+
+  /*============================================================================================================================
+  | VIEW MODEL: TOPIC SEARCH RESULT
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Provides a strongly-typed data transfer object for reporting an individual match discovered and returned via the <see
+  ///   cref="TopicSearchController"/>.
+  /// </summary>
+  public record TopicSearchResult {
+
+    /*==========================================================================================================================
+    | KEY
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   The key of the <see cref="AttributeRecord.Key"/> discovered.
+    /// </summary>
+    public string Key { get; init; }
+
+    /*==========================================================================================================================
+    | MATCH
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides the string that matched the supplied query.
+    /// </summary>
+    public string Match { get; init; }
+
+    /*==========================================================================================================================
+    | REPLACE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Provides the result of the replacement expression, if provided.
+    /// </summary>
+    public string Replace { get; init; }
+
+  } // Class
+} // Namespace
