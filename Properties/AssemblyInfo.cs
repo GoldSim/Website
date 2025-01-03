cc481ed68db233c9bc638edb73ee739c43b264cb
JeremyCaney <Jeremy.Caney@Ignia.com>
2025-01-03 01:10:34 -0800

    Removed generic justifications, labels

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -35,8 +35,8 @@ using System.Runtime.InteropServices;
 >===============================================================================================================================
 | Declare suppressions to code analysis rules that should be applied.
 \-----------------------------------------------------------------------------------------------------------------------------*/
-[assembly: SuppressMessage("Design", "CA1024:Use properties where appropriate", Justification = "Intentional design")]
-[assembly: SuppressMessage("Design", "CA1812", Justification = "Intentional design")]
+[assembly: SuppressMessage("Design", "CA1024")]
+[assembly: SuppressMessage("Design", "CA1812")]
 [assembly: SuppressMessage("Maintainability", "CA1515", Scope = "NamespaceAndDescendants", Target = "~N:GoldSim.Web.Components")]
 [assembly: SuppressMessage("Maintainability", "CA1515", Scope = "NamespaceAndDescendants", Target = "~N:GoldSim.Web.Controllers")]
 [assembly: SuppressMessage("Maintainability", "CA1515", Scope = "NamespaceAndDescendants", Target = "~N:GoldSim.Web.Models")]
