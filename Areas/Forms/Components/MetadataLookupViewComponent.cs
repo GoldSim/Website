b91e59894800c607a9a973fc9998f3f87bf48c10
Jeremy Caney <Jeremy.Caney@Ignia.com>
2025-11-13 16:34:06 -0800

    Remove redundant `else` statement

--- Areas/Forms/Components/MetadataLookupViewComponent.cs
+++ Areas/Forms/Components/MetadataLookupViewComponent.cs
@@ -107,7 +107,7 @@ namespace GoldSim.Web.Areas.Forms.Components {
           $"The {aspFor.Metadata.PropertyName} must be decorated with the [Metadata()] attribute."
         );
       }
-      else if (metadataList.Count is 0) {
+      if (metadataList.Count is 0) {
         throw new InvalidOperationException(
           $"The {aspFor.Metadata.PropertyName} is bound to the {metadataAttribute.Key} metadata, but the lookup list " +
           $"contains no topics."
