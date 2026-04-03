ce11d23b524f5ede4ca747b5b24e8e3dc72a0704
JeremyCaney <Jeremy.Caney@Ignia.com>
2026-04-03 13:43:42 -0700

    Added Datacor users to website authentication
    Ensure that users on @datacor.com are able to authenticate via the GoldSim website. We'll need to revisit authorization in a future update, as currently there's no authorization tools on the website.

--- Program.cs
+++ Program.cs
@@ -44,7 +44,9 @@ builder.Services.AddAuthentication(options => {
       //Ignia users
       "https://login.microsoftonline.com/10dcd9d4-80f7-47c8-ad5a-7efddcd5f868/v2.0",
       //GoldSim users
-      "https://login.microsoftonline.com/abfc6769-97de-4dc7-8284-0ecc2fac5cfc/v2.0"
+      "https://login.microsoftonline.com/abfc6769-97de-4dc7-8284-0ecc2fac5cfc/v2.0",
+      // Datacor users
+      "https://login.microsoftonline.com/646dd2cc-6f09-44bf-9cf6-869b33ffae59/v2.0"
     ]
   };
 })
