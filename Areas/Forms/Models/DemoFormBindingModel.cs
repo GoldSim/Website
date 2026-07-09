e8d438f53a6e347f5006582f054f3da4b6ce7e09
Jeremy Caney <Jeremy.Caney@Ignia.com>
2026-07-09 15:57:23 -0700

    Moved `Province` from `Contact` to form models
    Recently, I added the `Province` property to the `Contact` data model (04cb47d9). This introduced a conflict with the existing `Address.Province` property, which overwrites the value when saving it to OnTopic.
    
    Unfortunately, the cleanest fix for this is to just move `Province` from `Contact` and into the individual `TrialFormBindingModel` and `DemoFormBindingModel`. (There are other options, like creating a one-off `Province` record, but that adds more complexity than it's worth here.)

--- Areas/Forms/Models/DemoFormBindingModel.cs
+++ Areas/Forms/Models/DemoFormBindingModel.cs
@@ -16,6 +16,20 @@ namespace GoldSim.Web.Areas.Forms.Models {
   public sealed record DemoFormBindingModel : ExtendedProfile {
 
     /*==========================================================================================================================
+    | PROPERTY: PROVINCE
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Gets or sets the province (or state in America).
+    /// </summary>
+    /// <remarks>
+    ///   This cannot be defined on <see cref="Contact"/> because that causes a conflict with <see cref="Address.Province"/>.
+    /// </remarks>
+    [Required]
+    [StringLength(255)]
+    [Display(Name="State/Province")]
+    public string Province { get; set; }
+
+    /*==========================================================================================================================
     | PROPERTY: OTHER TOOLS
     \-------------------------------------------------------------------------------------------------------------------------*/
     /// <summary>
