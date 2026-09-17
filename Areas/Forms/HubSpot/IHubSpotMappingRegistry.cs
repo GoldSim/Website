9cc66c6843314c3a7ab7e5547a6813c29165ecd7
Jeremy Caney <Jeremy.Caney@Ignia.com>
2026-09-17 16:56:45 -0700

    Add `IHubSpotMappingRegistry` interface
    Defines the contract for looking up a `HubSpotFormManifest` (01b4d047) by its form identifier via the `TryGetManifest()` method. Implementations will be responsible for loading and validating the manifest JSON at startup; this commit adds only the interface.

---
+++ Areas/Forms/HubSpot/IHubSpotMappingRegistry.cs
@@ -0,0 +1,35 @@
+﻿/*==============================================================================================================================
+| Author        Ignia, LLC
+| Client        Goldsim
+| Project       Website
+\=============================================================================================================================*/
+
+namespace GoldSim.Web.Areas.Forms.HubSpot {
+
+  /*============================================================================================================================
+  | INTERFACE: HUBSPOT MAPPING REGISTRY
+  \---------------------------------------------------------------------------------------------------------------------------*/
+  /// <summary>
+  ///   Defines a service responsible for loading and providing access to <see cref="HubSpotFormManifest"/> instances, keyed by
+  ///   the form identifier.
+  /// </summary>
+  public interface IHubSpotMappingRegistry {
+
+    /*==========================================================================================================================
+    | METHOD: TRY GET MANIFEST
+    \-------------------------------------------------------------------------------------------------------------------------*/
+    /// <summary>
+    ///   Attempts to retrieve the <see cref="HubSpotFormManifest"/> associated with the given <paramref name="formIdentifier"
+    ///   />.
+    /// </summary>
+    /// <param name="formIdentifier">The identifier of the form to retrieve a manifest for (e.g., <c>TrialForm</c>).</param>
+    /// <param name="manifest">
+    ///   Contains the matching <see cref="HubSpotFormManifest"/>, if one was found; otherwise, <c>null</c>.
+    /// </param>
+    /// <returns>
+    ///   <c>true</c> if a manifest was found for the given <paramref name="formIdentifier"/>; otherwise, <c>false</c>.
+    /// </returns>
+    bool TryGetManifest(string formIdentifier, out HubSpotFormManifest manifest);
+
+  } //Interface
+} //Namespace
